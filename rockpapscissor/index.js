fa7868088abb5662b2efab33475108945edc5e67
deepanshu <deepanshusharma981098@gmail.com>
2026-01-15 11:40:15 +0530

    update

--- rockpapscissor/index.js
+++ rockpapscissor/index.js
@@ -1,4 +1,5 @@
 function game (user,computer){ 
+    if(user ===computer) return "draw"
     if(user ==="rock" ,computer === "scissor" ) return "user"
     if(user === "scissor" ,computer === "paper") return "user"
     if(user === "paper" , computer === "rock") return "user"
