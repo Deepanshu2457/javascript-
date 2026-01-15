bcdaddaf61a0413c00c3624289a61bad81771c12
deepanshu <deepanshusharma981098@gmail.com>
2026-01-15 11:42:29 +0530

    update

--- rockpapscissor/index.js
+++ rockpapscissor/index.js
@@ -1,8 +1,9 @@
 function game (user,computer){ 
-    if(user ===computer) return "draw"
-    if(user ==="rock" ,computer === "scissor" ) return "user"
-    if(user === "scissor" ,computer === "paper") return "user"
-    if(user === "paper" , computer === "rock") return "user"
-
+    if(user ===computer) return "draw";
+    if(user ==="rock" ,computer === "scissor" ) return "user";
+    if(user === "scissor" ,computer === "paper") return "user";
+    if(user === "paper" , computer === "rock") return "user";
+  
+    return "computer";
 }
-console.log(game("paper","rock"))
+console.log(game("paper","rock"));
