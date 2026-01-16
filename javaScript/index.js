45b4b01bff33d918afcba886b5eb873607c3beee
deepanshu <deepanshusharma981098@gmail.com>
2026-01-16 14:34:37 +0530

    evenOdd

--- javaScript/index.js
+++ javaScript/index.js
@@ -99,8 +99,23 @@
 
 
 
-let i = 10
-while(i >=1){
-console.log(i)
-i--
+// let i = 10
+// while(i >=0){
+// console.log(i)
+// i--
+// }
+
+for(let i = 1 ; i <=20; i++){
+    if(i %2 ===0){
+console.log(i);
+    }
+    
+}
+
+
+for(let i = 1 ; i <=20; i++){
+    if(i %2 ===1){
+console.log(i);
+    }
+    
 }
