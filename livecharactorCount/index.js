1db4a0160a9ec2cf2b6f633e625eaf61000ea426
deepanshu <deepanshusharma981098@gmail.com>
2026-03-22 00:29:44 +0530

    update

---
+++ livecharactorCount/index.js
@@ -0,0 +1,6 @@
+let inp = document.querySelector("input")
+let span= document.querySelector("span")
+
+inp.addEventListener("input", ()=>{
+ span.textContent = inp.value.length                                                                                                                                                                                                                                                                                                                                                                                                                    
+})
