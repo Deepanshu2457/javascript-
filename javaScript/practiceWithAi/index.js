f8b310db804ca7ed0b588bb78f76463bd8695045
deepanshu <deepanshusharma981098@gmail.com>
2026-05-07 17:56:59 +0530

    update

---
+++ javaScript/practiceWithAi/index.js
@@ -0,0 +1,35 @@
+Q1
+let name ;
+let age ;
+let isStudent ; 
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+Q2
+let x = "100";
+let y = 100;
+
+console.log(typeof(x));
+console.log(typeof(y));
+
+Q3
+let a = 5
+let b = 10
+a = a+b
+b = a-b
+a = a-b
+console.log(a);
+console.log(b);
+
+Q4
+let a ;
+console.log(a);
+
+Q5
+
+
+console.log(typeof(null));
+console.log(typeof(undefined));
+
