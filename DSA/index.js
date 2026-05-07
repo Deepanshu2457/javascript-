95d0362344a9c1b0e1e4b46feb3c8abb9a8dac88
deepanshu <deepanshusharma981098@gmail.com>
2026-05-07 17:35:08 +0530

    update

--- DSA/index.js
+++ DSA/index.js
@@ -2680,13 +2680,43 @@
 //     }
 // }
 
-let arr = [1,2,2,3,1]
+// let arr = [1,2,2,3,1]
 
-let result = []
-for(let i=0; i<arr.length; i++){
-    if(!result.includes(arr[i])){
-      result.push(arr[i])
-    }
+// let result = []
+// for(let i=0; i<arr.length; i++){
+//     if(!result.includes(arr[i])){
+//       result.push(arr[i])
+//     }
   
+// }
+// console.log(result);
+
+// let arr = [1,2,3,4,5]
+// let right = arr.length -1
+// let left = 0
+// while(left<right){
+//     let temp = arr[left]
+//      arr[left] = arr[right]
+//     arr[right]=temp
+     
+//    left++
+//    right--
+
+//     }
+
+// console.log(arr);
+
+
+let arr=[1,2,3,4,5]
+let left = 0
+let right = arr.length -1
+
+while(left < right){
+    let temp = arr[left]
+    arr[left] = arr[right]
+    arr[right] = temp
+
+    left++
+    right--
 }
-console.log(result);
+console.log(arr); 
