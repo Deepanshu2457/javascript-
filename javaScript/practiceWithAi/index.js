c904738093413db096a4f6a3b728c81ec781d6c6
deepanshu <deepanshusharma981098@gmail.com>
2026-05-13 18:38:32 +0530

    update

--- javaScript/practiceWithAi/index.js
+++ javaScript/practiceWithAi/index.js
@@ -227,50 +227,213 @@
 // }
 // console.log(count);
 
-// day 5
+// day 5  function => Function is reusable block of code used to perform a task and reduce repetition.
 // Q1
-function add(a,b){
-    return a + b
-}
-let result = add(4,5)
-console.log(result);
-// Q2
-function evenNum(num){
-    if(num % 2===0){
-        return true
-    }else{
-        return false
-    }
-}
-console.log(evenNum(6));
+// function add(a,b){
+//     return a + b
+// }
+// let result = add(4,5)
+// console.log(result);
+// // Q2
+// function evenNum(num){
+//     if(num % 2===0){
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+// console.log(evenNum(6));
 // Q3
-function factorial(num){
-    
-    return  num * (num * num) 
-}
-console.log(factorial(5));
+// function factorial(num){
+//     let fact = 1
+//     for(let i=1; i<=num; i++){
+//         fact = fact * i
+//     }
+//     return fact
+// }
+// console.log(factorial(5));
+
+// // Q4
+
+// function isPrime(num){
+//    if(num <= 1) return false
+
+//    for(let i=2; i<num;i++){
+//     if(num % i ===0){
+//         return false
+//     }
+//    }
+//   return true
+// }
+// console.log(isPrime(10));
+
+// function reverseNum(num){
+//     let rev =0
+//     while(num>0){
+//         let digit = num % 10
+//         rev = rev * 10 + digit
+//         num = Math.floor(num/10)
+//     }
+//     return rev
+// }
+// console.log(reverseNum(1234));
+
+// day 5 Array
+// Q1
+// let arr = [10,20,30,40,50]
+
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// Q2
+// let arr = [ 5,10,15,20]
+// let sum = 0
+// for(let i=0;i<arr.length; i++){
+//    sum = sum + arr[i]
+// }
+// console.log(sum);
+
+// Q3
+// let arr = [ 12, 45, 7,89, 23]
+
+// let largest = arr[0]
+// for(let i =0; i<arr.length ; i++){
+//     if(arr[i] > largest){
+//         largest = arr[i]
+//     }
+// }
+// console.log(largest);
 
 // Q4
 
-function isPrime(num){
-   if(num <= 1) return false
+//  let arr = [ 12, 45, 7,89, 23]
 
-   for(let i=2; i<num;i++){
-    if(num % i ===0){
-        return false
-    }
-   }
-  return true
-}
-console.log(isPrime(10));
+//  let small = arr[0]
 
-function reverseNum(num){
-    let rev =0
-    while(num>0){
-        let digit = num % 10
-        rev = rev * 10 + digit
-        num = Math.floor(num/10)
-    }
-    return rev
+//  for(let i = 0 ; i<arr.length; i++){
+//     if(arr[i] < small){
+//         small = arr[i]
+//     }
+//  }
+// console.log(small);
+
+// Q5
+
+// let arr = [2, 5, 8, 11, 14, 17];
+
+// let count = 0
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]%2 ===0){
+//         count++
+//     }
+// }
+// console.log(count);
+
+// Q6
+
+// let arr=[1,2,3,4,5]
+// let left =0
+// let right = arr.length -1
+
+
+// while(left < right){
+//     let temp = arr[left]
+//     arr[left] = arr[right]
+//     arr[right] = temp
+
+//     left++
+//     right--
+// }
+// console.log(arr);
+
+// day 7 Strings
+Q1
+
+let str = "hello"
+let rev =" "
+
+for(let i = str.length -1 ; i>=0; i--){
+    rev = rev + str[i]
 }
-console.log(reverseNum(1234));
+console.log(rev);
+
+Q2
+let str = "madam"
+
+function isPalindrome(str){
+    let check = ""
+  for(let i= str.length -1 ; i>=0; i--){
+   check = check + str[i]
+  }
+  if(str === check){
+    return true
+  } else{ 
+    return false
+  }
+}
+console.log(isPalindrome("madam"));
+
+
+function palindrome(str){
+    let left = 0
+    let right = str.length -1
+
+    while(left < right){
+        if(str[left] !== str[right]){
+               return false
+        }
+        left++
+        right--
+    }
+    return true
+
+}
+
+console.log(palindrome("madam"));
+console.log(palindrome("deepa"));
+
+
+Q3
+
+let str = "javascript"
+let count = 0
+
+for(let i=0; i<str.length; i++){
+    if("aeiou".includes(str[i])){
+        count++
+}
+}
+console.log(count);
+
+for(let i=0; i<str.length; i++){
+    if(str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u"){
+     count++
+}
+}
+console.log(count);
+
+Q4
+  note : str ki value ku change nhhi hui .. dusre varible mai store ku karna pada .. dusre variable value change ho gyi ( asa ku )
+let str = "hello world"
+  let upper = str.toUpperCase()
+console.log(upper);
+console.log(str); 
+
+
+
+Q5
+
+let str = "HELLO WORLD"
+let lower = str.toLowerCase()
+console.log(lower);
+
+Q6
+
+let str = " j a v a s c r i p t ";
+let result = ""
+for(let i=0; i<str.length; i++){
+    if(str[i] !== " ")
+   result = result + str[i]
+}
+console.log(result);
