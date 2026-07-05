1c7754d5da166a9a3392a87cc5f4697a4eb7433d
deepanshu <deepanshusharma981098@gmail.com>
2026-07-05 15:47:49 +0530

    update

--- DSA/index.js
+++ DSA/index.js
@@ -4128,14 +4128,20 @@
 
 // Q 11
 
-let arr = [1,1,2,3,2,4,5,4]
+// let arr = [1,1,2,3,2,4,5,4]
 
+// let result = []
+
+// for(let i=0; i<arr.length; i++){
+//  if(!result.includes(arr[i])){
+//     result.push(arr[i])
+//  }
+// }
+// console.log(result);
+
+let arr = [10,20,30,40,50]
 let result = []
-
-for(let i=0; i<arr.length; i++){
- if(!result.includes(arr[i])){
+for(let i=arr.length-1 ; i>=0; i--){
     result.push(arr[i])
- }
 }
 console.log(result);
-
